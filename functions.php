185e7615bfd65ce0de496267a5a7aa70cd32b1c7
Ea Rist <earis@greeny.cs.tlu.ee>
2015-10-07 13:10:49 +0300

    ei näita tabelis kustutatud ridu

--- functions.php
+++ functions.php
@@ -6,23 +6,53 @@
 	
 	function getCarData(){
 		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
-		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates");
+		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates WHERE deleted IS NULL");
 		$stmt->bind_result($id, $user_id, $number_plate, $color);
 		$stmt->execute();
 		
-		$row = 0;
+		//tekitan tühja massiivi, kus edaspidi hoian objekte
+		$car_array = array ();
 		
 		//tee midagi seni, kuni saame andmebaasist ühe rea andmeid
 		while($stmt->fetch()){
 			//seda siin sees tehakse nii mitu korda kui on ridu
-			echo $row." ".$number_plate."<br>";
-			$row = $row +1; //või teine kirjapilt- $row+= 1; $row++;
+			
+			//tekitan objekti, kus hakkan hoidma väärtusi
+			$car = new StdClass();
+			$car->id = $id;
+			$car->plate =$number_plate;
+			$car->user_id=$user_id;
+			$car->color=$color;
+			
+			//lisan massiivi ühe rea juurde
+			array_push($car_array, $car);
+			//var dump ütleb muutuja tüübi ja sisu
+			//echo "<pre>";
+			//var_dump($car_array);
+			//echo "</pre><br>";
+			
 		}
+		//tagastan massiivi, kus kõik read sees
+		return $car_array;
 		
 		$stmt->close();
 		$mysqli->close();
 		
 	}
-	//käivitan funktsiooni
-	getCarData();
+	function deleteCar($id){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("UPDATE car_plates SET deleted=NOW() WHERE id=?");
+		$stmt->bind_param("i", $id);
+		if($stmt->execute()){
+			//sai kustutatud, kustutame aadressirea tühjaks
+			header("Location: table.php");
+			
+		}
+		$stmt->close();
+		$mysqli->close();
+		
+	}
+	
+	
 ?>
--- table.php
+++ table.php
@@ -1,1 +1,45 @@
-table
+<?php
+	require_once("functions.php");
+	
+	//kas kustutame, ?delete = vastav id mida kustutada on aadressireal
+	if(isset($_GET["delete"])){
+		echo "Kustutame id" .$_GET["delete"];
+		//käivitan funktsiooni, saadan kaasa id
+		deleteCar($_GET["delete"]);
+	
+	}
+	
+	//käivitan funktsiooni
+	$car_array = getCarData();
+	
+	//trükin välja esimese auto
+	//echo $car_array[0]->id." ".$car_array[0]->plate;
+	
+?>
+
+<h2>Tabel</h2>
+<table border="1">
+	<tr>
+		<th>Id</th>
+		<th>Numbrimärk</th>
+		<th>User id</th>
+		<th>Värv</th>
+		<th>X</th>
+	</tr>
+
+	<?php
+		//trükime välja read
+		//massiivi pikkus count()
+		for($i = 0; $i < count($car_array); $i++){
+			//echo $car_array[$i]->id;
+			echo "<tr>";
+			echo "<td>".$car_array[$i]->id."</td>";
+			echo "<td>".$car_array[$i]->plate."</td>";
+			echo "<td>".$car_array[$i]->user_id."</td>";
+			echo "<td>".$car_array[$i]->color."</td>";
+			echo "<td><a href='?delete=".$car_array[$i]->id."'>X</a></td>";
+			echo "</tr>";
+		}
+	
+	?>
+</table>
