b14ad6573cbab7fa0cfac11288f5427c77992b54
Ea Rist <earis@greeny.cs.tlu.ee>
2015-10-07 11:16:07 +0300

    echo row number and car plate

--- functions.php
+++ functions.php
@@ -1,1 +1,28 @@
-funktsioonid
+<?php
+
+	//Loome ühenduse andmebaasiga
+	require_once("../config_global.php");
+	$database = "if15_earis_3";
+	
+	function getCarData(){
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		$stmt = $mysqli->prepare("SELECT id, user_id, number_plate, color FROM car_plates");
+		$stmt->bind_result($id, $user_id, $number_plate, $color);
+		$stmt->execute();
+		
+		$row = 0;
+		
+		//tee midagi seni, kuni saame andmebaasist ühe rea andmeid
+		while($stmt->fetch()){
+			//seda siin sees tehakse nii mitu korda kui on ridu
+			echo $row." ".$number_plate."<br>";
+			$row = $row +1; //või teine kirjapilt- $row+= 1; $row++;
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+	}
+	//käivitan funktsiooni
+	getCarData();
+?>
